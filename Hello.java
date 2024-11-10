ebfdea71d455ab95d7802d4331f8e7ab1216cd07
shamima <shamima@gmail.com>
2024-11-10 11:19:00 -0600

    First Commit

---
+++ Hello.java
@@ -0,0 +1,15 @@
+package Git_Proj;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Hello Rupali");
+		System.out.println("Acknowledged");
+		System.out.println("Hello Bangladesh");
+		
+		
+
+	}
+
+}
